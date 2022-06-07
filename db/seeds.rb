8bf52408c051eabe93877f1bbe2f059b748cd74b
Maco <maco@maco.com>
2022-06-07 14:15:46 +0200

    Add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.create(
+  email: 'jerko.culina@example.com', 
+  password: 'thisisvalidpassword', 
+  password_confirmation: 'thisisvalidpassword', 
+  available_cents: 1000_00,
+  auth_token: 'b74dCaZwiEUmV0kE03tvksECtfmDq3xMU856FCAVWRJ5GABWbOSMy5FHw6yy'
+)
+
+category_1 = Category.create(
+  user: user,
+  name: 'Rezije'
+)
+
+category_2 = Category.create(
+  user: user,
+  name: 'Zabava'
+)
+
+category_1_expenses = Expense.create(
+  category: category_1,
+  amount_cents: 100_00,
+  title: "Struja"
+)
+
+category_2_expenses = Expense.create(
+  category: category_2,
+  amount_cents: 50_00,
+  title: "Kino"
+)
